99866ef5bb2bd4ed306f070256d6d27d751d35cf
jorgitodf <jspaiva.1977@gmail.com>
2017-11-21 11:33:10 -0200

    Alteração Imagem Error 404

--- application/views/errors/error_404.php
+++ application/views/errors/error_404.php
@@ -1,12 +1,14 @@
 
 <div class="container-fluid col-md-sm col-md-12 col-lg-12 div_page_principal">
     <div class="row-fluid">
-        <section class="jumbotron col-md-8 col-sm-8 col-lg-8" id="sec_erro_404">
-            <figure class="thumbnail">
-                <img class="img-responsive img_erro_404" src="/assets/imgs/404-error.jpg" />
-                <p><?php echo !empty($message) ? $message : ""; ?></p>
-            </figure>
-            <div class="div-erro-404">
+        <section class="col-xs-8 col-md-8 col-sm-8 col-lg-8" id="sec_erro_404">
+            <div class="row">
+                <figure class="img-responsive">
+                    <img class="imagem_error_404 col-xs-6 col-md-6 col-sm-6 col-lg-6" />
+                    <p><?php echo !empty($message) ? $message : ""; ?></p>
+                </figure>
+            </div>
+            <div class="row col-xs-10 col-md-10 col-sm-10 col-lg-10">
                 <a href="/home" class="btn btn-success">Voltar a Página Inicial</a>
             </div>
         </section>
